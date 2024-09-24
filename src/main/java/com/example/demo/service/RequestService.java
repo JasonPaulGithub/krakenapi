8abe78caf00dc618a44daefa225f11980d3eb2d7
Jason Paul <gapspace@engineer.com>
2024-09-24 23:55:57 +0100

    feat: move logic out of controller to service

---
+++ src/main/java/com/example/demo/service/RequestService.java
@@ -0,0 +1,54 @@
+package com.example.demo.service;
+
+import com.example.demo.models.Device;
+import com.example.demo.models.Outage;
+import com.example.demo.models.SiteInfo;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class RequestService {
+
+    private static final String CUT_OFF_DATE = "2022-01-01T00:00:00.000Z";
+    private KrakenService krakenService;
+
+    public ResponseEntity<?> filterOutages(String requestedSite) {
+        return ReturnResponse(filteredOutages(requestedSite));
+    }
+
+    private List<Outage> filteredOutages(String requestedSite) {
+        List<Outage> filteredOutages = new ArrayList<>();
+        SiteInfo siteInfo = krakenService.getSiteInfo(requestedSite);
+        List<Outage> outages = krakenService.getOutages();
+        for (Device device : siteInfo.getDevices()) {
+            for (Outage outage : outages) {
+                Instant outageBegin = Instant.parse(outage.getBegin());
+                if (device.getId().equals(outage.getId()) && outageBegin.isBefore(Instant.parse(CUT_OFF_DATE))) {
+                    filteredOutages.add(outage);
+                }
+            }
+        }
+        return filteredOutages;
+    }
+
+    private ResponseEntity<?> ReturnResponse(List<Outage> filteredOutageList) {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        try {
+            String json = ow.writeValueAsString(filteredOutageList);
+            return new ResponseEntity<>("The following outages have been posted:\n\n" + json, HttpStatus.OK);
+        } catch (JsonProcessingException e) {
+            return new ResponseEntity<>("Could not map to Json.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
